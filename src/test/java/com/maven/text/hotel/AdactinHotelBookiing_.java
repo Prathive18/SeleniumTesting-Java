69f9e2bf4c2bef0997a1b8377104c5c57e8da57a
Prathive Kc <sarasuprithiv@gmail.com>
2025-06-11 22:18:53 +0530

    Initial Commit

---
+++ src/test/java/com/maven/text/hotel/AdactinHotelBookiing_.java
@@ -0,0 +1,15 @@
+package com.maven.text.hotel;
+
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+public class AdactinHotelBookiing_ {
+     public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("<!-- https://mvnrepository.com/artifact/org.seleniumhq.selenium/selenium-java -->\r\n"
+				+ "<dependency>\r\n"
+				+ "    <groupId>org.seleniumhq.selenium</groupId>\r\n"
+				+ "    <artifactId>selenium-java</artifactId>\r\n"
+				+ "    <version>4.33.0</version>\r\n"
+				+ "</dependency>");
+	}
+}
